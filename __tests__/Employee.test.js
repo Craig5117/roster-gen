f680edd02291135b833fce078de3ef3bf74cb991
Craig5117 <craig.bennett5117@gmail.com>
2020-12-10 18:59:49 -0500

    Employee object created and test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,13 @@
 const Employee = require('../lib/Employee');
 
-test()
+test('Creates an Employee object', () => {
+    const employee = new Employee('Abe', 123, 'abe@email.com');
+    expect(employee.name).toBe('Abe');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+// expect(employee.getName)
+//     expect 
+//     expect
+//     expect 
