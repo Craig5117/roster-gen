c20634c87ed9baea7ab1e7cb1f3c36aaa2166ef5
Craig5117 <craig.bennett5117@gmail.com>
2020-12-11 11:04:42 -0500

    Employee.getName is working as expected

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,6 +7,10 @@ test('Creates an Employee object', () => {
     expect(employee.email).toEqual(expect.any(String));
 });
 
+test('Returns Employee name', () => {
+    const employee = new Employee('Bilbo', 456, 'bilbo@email.com');
+    expect(employee.getName()).toBe('Employee Name: Bilbo')
+})
 // expect(employee.getName)
 //     expect 
 //     expect
