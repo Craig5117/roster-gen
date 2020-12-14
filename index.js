ae13f15ec92d2b2c291705da7f9c4a9f9e33f2ae
Craig5117 <craig.bennett5117@gmail.com>
2020-12-13 20:00:49 -0500

    eliminated a comma from the HTML by joining the array

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const questionSwitch = require('./lib/Questions');
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const questionSwitch = require("./lib/Questions");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 const appLogo = `
 ██████╗  ██████╗ ███████╗████████╗███████╗██████╗     
 ██╔══██╗██╔═══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗    
@@ -21,99 +21,97 @@ const appLogo = `
      ╚═════╝ ╚══════╝╚═╝  ╚═══╝                       
                                                       \n`;
 
-
-
-
+// Array that stores each team member object
 let teamMembers = [];
-
-// const PUBLIC_DIR = path.resolve(__dirname, "public");
-// const publicPath = path.join(PUBLIC_DIR, "team.html")
-
-/*
-  function buildTeam() {
-    // Create the output directory if the output path doesn't exist
-    if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR)
-    }
-    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+// This function builds the team members array and writes the file
+function appMenu() {
+  // Sends manager to questionSwitch and returns user input  prompts
+  function buildManager() {
+    console.log(appLogo);
+    console.log("Let's get started building your team roster!");
+    return questionSwitch("manager");
   }
-*/
+  // Sends engineer to questionSwitch and returns user input from prompts
+  function buildEngineer() {
+    return questionSwitch("engineer");
+  }
+  // Sends intern to questionSwitch and returns user input from prompts
+  function buildIntern() {
+    return questionSwitch("intern");
+  }
+  // Activates the main menu which recursively calls itself each time a team member is created until the user quits
+  function mainMenu() {
+    console.log(appLogo);
+    return inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "menuChoice",
+          message: "Add Your Team Members",
+          choices: ["1. Add Engineer", "2. Add Intern", "3. Quit"],
+        },
+      ])
+      .then((selection) => {
+        switch (selection.menuChoice) {
+          case "1. Add Engineer":
+            // Creates an Engineer based on user input and returns to the main menu
+            buildEngineer()
+              .then((engineerData) => {
+                return new Engineer(
+                  engineerData.name,
+                  engineerData.id,
+                  engineerData.email,
+                  engineerData.github
+                );
+              })
+              .then((engineer) => {
+                teamMembers = [...teamMembers, engineer];
+                mainMenu();
+              });
+            break;
+          case "2. Add Intern":
+            // Creates an intern based on user input and returns to the main menu
+            buildIntern()
+              .then((internData) => {
+                return new Intern(
+                  internData.name,
+                  internData.id,
+                  internData.email,
+                  internData.school
+                );
+              })
+              .then((Intern) => {
+                teamMembers = [...teamMembers, Intern];
+                mainMenu();
+              });
+            break;
+          default:
+            // Sends the teamMembers array to page-template.js and recieves HTML response
+            generatePage(teamMembers).then((teamHtml) => {
+              console.log("Writing HTML. Check it out at dist/index.html");
+              // writes the file based on the HTML received from generatePage
+              return fs.writeFile("./dist/index.html", teamHtml, (err) => {
+                if (err) console.log(err);
+              });
+            });
+        }
+      });
+  }
 
-function appMenu() {    
-    function buildManager() {
-        console.log(appLogo)
-        console.log("Let's get started building your team roster!")
-        return questionSwitch('manager');    
-    }
-
-    function buildEngineer() {
-        return  questionSwitch('engineer')
-    }
-
-    function buildIntern() {
-        return questionSwitch('intern');
-    }
-
-    function mainMenu() {
-    console.log(appLogo)
-        return inquirer.prompt([
-            {
-                type: 'list',
-                name: 'menuChoice',
-                message: 'Add Your Team Members',
-                choices: ['1. Add Engineer', '2. Add Intern', '3. Quit']
-            }
-        ]).then(selection => {
-            switch (selection.menuChoice) {
-                case '1. Add Engineer':
-                    buildEngineer().then(engineerData => {
-                        return new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github)
-                    }).then(engineer => {
-                       teamMembers = [...teamMembers, engineer]
-                       mainMenu(); 
-                    });
-                    break;      
-                case '2. Add Intern':
-                    buildIntern().then(internData => {
-                        return new Intern(internData.name, internData.id, internData.email, internData.school)
-                    }).then(Intern => {
-                       teamMembers = [...teamMembers, Intern]
-                       mainMenu(); 
-                    });      
-                    break
-                default:
-                   generatePage(teamMembers).then(teamHtml => {
-                    console.log('Generating Roster...')
-                    return fs.writeFile('./dist/index.html', teamHtml, err => {
-                        if (err) console.log(err)
-                    });
-                   })
-            }
-        })
-    }
-    
-    
-    buildManager().then(managerData => {
-        return new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber);
-    }).then(manager => {
-        teamMembers = [...teamMembers, manager];
-        mainMenu();
+  // Creates a new manager based on user input and initiates the main menu
+  buildManager()
+    .then((managerData) => {
+      return new Manager(
+        managerData.name,
+        managerData.id,
+        managerData.email,
+        managerData.officeNumber
+      );
     })
-
-    
+    .then((manager) => {
+      teamMembers = [...teamMembers, manager];
+      mainMenu();
+    });
 }
 
-
 appMenu();
-
-// (async function app() {
-//     try {
-//         const team = await appMenu();
-//         // await console.log(teamdata);
-        
-//     }
-//     catch (error) {
-//         if (error) console.log(error) 
-//     }
-
-// })();
--- src/page-template.js
+++ src/page-template.js
@@ -1,48 +1,50 @@
+// This stores the individual html blocks for each member
 let html = [];
-const generateRoster = teamMembers => {
-    // const { getGithub, getId, getOfficeNum, getRole, getSchool, getEmail, getName, generateRoster } = teamMembers;
-    // console.log(generateRoster, typeof generateRoster);
-    teamMembers.map(member => {
-        let role = member.getRole();
-        let icon;
-        let spec;
-        let name = member.getName();
-        let id = member.getId();
-        let email = member.getEmail();
-        const setIcon = function(role) {
-            switch (role) {
-                case 'Manager':
-                    icon = `fas fa-mug-hot`;
-                    return icon;
-                case 'Engineer':
-                    icon = `fas fa-glasses`;
-                    return icon;
-                case 'Intern':
-                    icon = `fas fa-user-graduate`
-                    return icon;
-            }
-        }
-        const setSpec = function() {
-            if (member.getGithub) {
-                spec = `<a href="https://github.com/${member.getGithub()}"><p class="p-4">GitHub: ${member.getGithub()}</p></a>`;
-                return spec;
-            }
-            else if (member.getSchool) {
-                spec = `<p class="p-4">${member.getSchool()}</p>`;
-                return spec;
-            }
-            else if (member.getOfficeNum) {
-                spec = `<p class="p-4">${member.getOfficeNum()}</p>`;
-                return spec;
-            }
-        }
-    
-        
-        const coalesced = `
+const generateRoster = (teamMembers) => {
+  // Loops over teamMembers array and creates an html object for each member
+  teamMembers.map((member) => {
+    let role = member.getRole();
+    let icon;
+    let spec;
+    let name = member.getName();
+    let id = member.getId();
+    let email = member.getEmail();
+    // Sets the icon for each member
+    const setIcon = function (role) {
+      switch (role) {
+        case "Manager":
+          icon = `fas fa-mug-hot`;
+          return icon;
+        case "Engineer":
+          icon = `fas fa-glasses`;
+          return icon;
+        case "Intern":
+          icon = `fas fa-user-graduate`;
+          return icon;
+      }
+    };
+    // Sets the special category for each member
+    const setSpec = function () {
+      if (member.getGithub) {
+        spec = `<a href="https://github.com/${member.getGithub()}"><p class="p-4">GitHub: ${member.getGithub()}</p></a>`;
+        return spec;
+      } else if (member.getSchool) {
+        spec = `<p class="p-4">${member.getSchool()}</p>`;
+        return spec;
+      } else if (member.getOfficeNum) {
+        spec = `<p class="p-4">${member.getOfficeNum()}</p>`;
+        return spec;
+      }
+    };
+
+    // Assembles the html block and adds it to the array
+    const coalesced = `
         <div class="card column is-one-quarter m-3 p-0">
             <div class="card-header is-block has-background-info p-4">
                 <h2 class="title is-3 has-text-white">${name}</h2>
-                <h3 class="has-text-white subtitle is-4"><i class="${setIcon(role)} mr-2"></i>${role}</h3>
+                <h3 class="has-text-white subtitle is-4"><i class="${setIcon(
+                  role
+                )} mr-2"></i>${role}</h3>
             </div>
             <div class="card-content has-background-light">
                 <div class="has-background-white">
@@ -51,44 +53,18 @@ const generateRoster = teamMembers => {
                     ${setSpec()}
                 </div>
             </div>
-        </div>`
+        </div>`;
 
-        return html = [...html, coalesced];
-    })
+    return (html = [...html, coalesced]);
+  });
+  // Joins html array and returns it to be added to the template below
+  return html.join(' ');
+};
 
-    return [html];
-
-    
-} 
-
-// function exportHTML (teamMembers) {return console.log(
-//     `
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <title>Your Team</title>
-//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css">
-//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-//     <link rel="stylesheet" href='./style.css'>
-// </head>
-// <body>
-//     <header class=" has-background-danger has-text-centered p-6">
-//         <h1 class="title is-2 has-text-white">MyTeam</h1>
-//     </header>
-//     <main class="is-flex is-justify-content-center">
-//         <div class="column is-four-fifths is-flex is-justify-content-center is-flex-wrap-wrap">
-//             ${generateRoster(teamMembers)}
-//         </div>
-//     </main>
-// </body>
-// </html>`)}
-
-module.exports = teamMembers => {
-    return   new Promise((resolve, reject) => {
-        const teamHtml = `
+module.exports = (teamMembers) => {
+  // New promise makes the .then on generatePage in index.js work
+  return new Promise((resolve, reject) => {
+    const teamHtml = `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -111,9 +87,12 @@ module.exports = teamMembers => {
             </main>
         </body>
         </html>`;
-        if (teamHtml) {
-            resolve(teamHtml);
-        }
-        else {reject()}
-    }) 
+    if (teamHtml) {
+      // Resolves and returns teamHTML to be written in index.js
+      console.log("Generating Roster...");
+      resolve(teamHtml);
+    } else {
+      reject();
+    }
+  });
 };
